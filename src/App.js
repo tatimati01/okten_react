cb65c08a0f7b947116cf4028f761e9823f1d018a
tatimati01 <tati.mati.work@gmail.com>
2022-01-13 15:31:22 +0200

    add classwork1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
-import
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+    const [launchesList, setLaunchesList] = useState();
+    useEffect(()=> {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(response => response.json())
+            .then(launches => {
+                setLaunchesList(launches.filter(mission => mission.launch_year !== '2020'));
+            });
+    },[]);
     return (
         <div>
-          
+            {
+                launchesList.map(value => <div key={value.flight_number}>
+                    {value.mission_name} ({value.launch_year} year)
+                    <img src={value.links.mission_patch_small} alt={value.mission_name}/>
+
+                </div>)
+            };
+
         </div>
     );
 };
