2da10bff2b5277f48e3c4e86930b93c8dce43d58
tatimati01 <tati.mati.work@gmail.com>
2022-01-22 12:24:27 +0200

    hw cars

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import css from "../../App.module.css";
 import {useForm} from "react-hook-form";
+import {carService} from "../../services/car.service";
 
 const Form = () => {
+    const [formError, setFormError] = useState({});
+
     const {register, handleSubmit, watch, formState: {errors}} = useForm();
 
-    const submit = (data) => {
-        console.log(data);
+    const submit = (car) => {
+        carService.create(car).then(value => console.log(value)).catch(error => {
+            setFormError(error.response.data)
+        })
     }
 
     return (
@@ -16,14 +21,17 @@ const Form = () => {
                 <div className={css.formItem}>
                     <label>Model:</label>
                     <input type='text' {...register('model')} defaultValue={''}/>
+                    {formError.model && <span>{formError.model[0]}</span>}
                 </div>
                 <div className={css.formItem}>
                     <label>Year:</label>
                     <input type='text' {...register('year')} defaultValue={''}/>
+                    {formError.year && <span>{formError.year[0]}</span>}
                 </div>
                 <div className={css.formItem}>
                     <label>Price:</label>
                     <input type='text' {...register('price')} defaultValue={''}/>
+                    {formError.price && <span>{formError.price[0]}</span>}
                 </div>
                 <button>Save</button>
             </form>
---
+++ src/validators/car.validator.js
@@ -0,0 +1,2 @@
+export const CarValidator = Joi.object()
+
