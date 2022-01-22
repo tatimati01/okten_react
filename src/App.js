e02de62115c94b83e9ac29c7c34d0f513b8036ff
tatimati01 <tati.mati.work@gmail.com>
2022-01-22 20:53:37 +0200

    hw3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import {Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Content from "./components/Content/Content";
 
 
 function App() {
 
 
-  return (
-    <div>
-      <div>
-        <NavBar
-      </div>
-      <div>
+    return (
+        <div>
+            <div>
+                <NavBar/>
+            </div>
+            <div>
+                <Content/>
 
-      </div>
-    </div>
-  );
+                <Routes>
+                    <Route path={'/users'}>
+                        <Route>
+
+                        </Route>
+                    </Route>
+                    <Route path={'/posts'}>
+
+                    </Route>
+                </Routes>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
+import css from '../../App.module.css'
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className={css.navbar}>
+            <h1><Link to={'/users'}>Users</Link></h1>
+            <h1><Link to={'/posts'}>Posts</Link></h1>
         </div>
     );
 };
